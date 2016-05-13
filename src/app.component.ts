28f555050adff11d0cb025d4e55c0d38fe130d8f
oGre <ogre@muppfarmen.se>
2016-05-13 10:20:21 +0200

    data binding

--- src/app.component.ts
+++ src/app.component.ts
@@ -5,10 +5,16 @@ import {Component} from "@angular/core";
     selector: 'my-app',
     template: `
     <h1>Min Angular2 App</h1>
+    <p>{{name}}</p>
+    <input type="text" [(ngModel)]="name"/>
+    <p [class.danger]="name && name.length > 10">{{name}}</p>
+    <button type="button" (click)="name=undefined">Rensa</button>
     `
 })
 
 export class AppComponent {
+    name:string='C.A.G Contactor';
+
     constructor() {
         console.log('test');
     }
