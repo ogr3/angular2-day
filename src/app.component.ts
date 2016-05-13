eef16762c6d894c701d0448925096f9f9e599d05
oGre <ogre@muppfarmen.se>
2016-05-13 15:21:42 +0200

    template

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,17 +4,11 @@ import {Routes, Router, ROUTER_DIRECTIVES} from "@angular/router";
 import {BeerService} from "./beer/beer.service";
 import {BeerAdminComponent} from "./beeradmin/beeradmin.component";
 import {TopListComponent} from "./toplist/toplist.component";
-
 @Component({
   selector: 'my-app',
   providers: [BeerService],
   directives: [ROUTER_DIRECTIVES],
-  template: `<nav>
-  <a [routerLink]="['/top-list']">Topplista</a>
-  <a [routerLink]="['/beer-admin']">Administration</a>
-</nav>
-<hr>
-<router-outlet></router-outlet>`
+  templateUrl: 'app.html'
 })
 @Routes([
   {path: '/top-list', component: TopListComponent},
