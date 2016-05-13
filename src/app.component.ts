a25a32343b8c644570b86acaefa03d645a4ea264
oGre <ogre@muppfarmen.se>
2016-05-13 11:18:12 +0200

    working

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,20 +1,32 @@
 import {Component} from "@angular/core";
+import {PartComponent} from "./part.component";
 
 
 @Component({
     selector: 'my-app',
+    directives: [PartComponent],
     templateUrl: 'beer.html'
 })
 
 export class AppComponent {
     name: string='C.A.G Contactor';
+    
     seconds: number = -1;
+    things: string[] = ['baka', 'kaka'];
+    nameSize: number = 0;
 
     constructor() {
         this.nextTimeout();
         console.log('test');
     }
 
+    addThing() {
+        this.things.push(this.name);
+    }
+
+    showSize(size) {
+        this.nameSize = size;
+    }
     nextTimeout() {
         this.seconds++;
         setTimeout(() => this.nextTimeout(), 1000);
---
+++ src/part.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Created by fredrik on 2016-05-13.
+ */
+import {Component, Input, EventEmitter, Output} from "@angular/core";
+
+@Component({
+    selector: 'part',
+    template: `
+    <h4>Text som kommer från @Input: {{text}}</h4>
+    <button type="button" (click)="calc()">Beräkna längd</button>
+    `
+})
+
+export class PartComponent {
+    @Input()
+    text: string;
+    
+    @Output()
+    size: EventEmitter<number> = new EventEmitter();
+    
+    calc() {
+        this.size.emit(this.text.length);
+    }
+}
