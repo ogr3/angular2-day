78ad88a7b140ae3a8f51ab9da3ce169a860201a0
oGre <ogre@muppfarmen.se>
2016-05-13 11:40:08 +0200

    egen fil

--- src/beerdetails/beerdetails.component.ts
+++ src/beerdetails/beerdetails.component.ts
@@ -3,18 +3,7 @@ import {Beer} from "./beer";
 
 @Component({
   selector: 'my-beer-details',
-  template: `
-<h2>Ölsort: {{beer.name}}</h2>
-<dl>
-  <dt>Beskrivning</dt>
-  <dd>{{beer.description}}</dd>
-</dl>
-
-<hr>
-Ange ölsort: <input type="text" [(ngModel)]="beer.name"><br><br>
-Karaktär: <input type="text" [(ngModel)]="beer.description"><br><br>
-<button type="button" (click)="like()">Yep, den är smarrig!</button>
-`
+  templateUrl: 'beerdetails/beerdetails.html'
 })
 export class BeerDetailsComponent {
   @Input()
